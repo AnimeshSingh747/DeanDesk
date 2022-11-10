4059082670f8d7fadd3bb27011091ed65b49a702
ANIMESH SINGH <csanimeshsingh747@gmail.com>
2022-11-10 19:23:50 +0530

    minor changes

--- app/src/main/java/com/example/cutmdesk/SchoolFragment.java
+++ app/src/main/java/com/example/cutmdesk/SchoolFragment.java
@@ -12,6 +12,6 @@ public class SchoolFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_school, container, false);
+        return inflater.inflate(R.layout.fragment_school, container, true);
     }
 }
