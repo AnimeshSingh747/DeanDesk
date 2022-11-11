c3b65f38664b2a7119d9fb8341676848bf53932d
ANIMESH SINGH <csanimeshsingh747@gmail.com>
2022-11-12 01:12:13 +0530

    Minor issues fixed

--- app/src/main/java/com/example/cutmdesk/SchoolFragment.java
+++ app/src/main/java/com/example/cutmdesk/SchoolFragment.java
@@ -13,46 +13,46 @@ import androidx.fragment.app.Fragment;
 public class SchoolFragment extends Fragment implements View.OnClickListener {
 
 
-    ImageView SOET1;
-    ImageView SOEVT2;
-    ImageView SOF3;
-    ImageView SOAS4;
-    ImageView SOMC5;
-    ImageView SOP6;
-    ImageView MSSSOA7;
-    ImageView SOABE8;
-    ImageView SOM9;
-    ImageView SOPAHS10;
-    ImageView SOFS11;
+    ImageView soet1;
+    ImageView sovet2;
+    ImageView sof3;
+    ImageView soas4;
+    ImageView somc5;
+    ImageView sop6;
+    ImageView msssoa7;
+    ImageView soabe8;
+    ImageView som9;
+    ImageView sopahs10;
+    ImageView sofs11;
 
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_school, container, false);
-        SOET1 = (ImageView) view.findViewById(R.id.SOET1);
-        SOEVT2 = (ImageView) view.findViewById(R.id.SoVET2);
-        SOF3 = (ImageView) view.findViewById(R.id.SOF3);
-        SOAS4 = (ImageView) view.findViewById(R.id.SOAS4);
-        SOMC5 = (ImageView) view.findViewById(R.id.SOMC5);
-        SOP6 = (ImageView) view.findViewById(R.id.SOP6);
-        MSSSOA7 = (ImageView) view.findViewById(R.id.MSSSOA7);
-        SOABE8 = (ImageView) view.findViewById(R.id.SOABE8);
-        SOM9 = (ImageView) view.findViewById(R.id.SOM9);
-        SOPAHS10 = (ImageView) view.findViewById(R.id.SOPAHS10);
-        SOFS11 = (ImageView) view.findViewById(R.id.SOFS11);
+        soet1 = (ImageView) view.findViewById(R.id.soet1);
+        sovet2 = (ImageView) view.findViewById(R.id.sovet2);
+        sof3 = (ImageView) view.findViewById(R.id.sof3);
+        soas4 = (ImageView) view.findViewById(R.id.soas4);
+        somc5 = (ImageView) view.findViewById(R.id.somc5);
+        sop6 = (ImageView) view.findViewById(R.id.sop6);
+        msssoa7 = (ImageView) view.findViewById(R.id.msssoa7);
+        soabe8 = (ImageView) view.findViewById(R.id.soabe8);
+        som9 = (ImageView) view.findViewById(R.id.som9);
+        sopahs10 = (ImageView) view.findViewById(R.id.sopahs10);
+        sofs11 = (ImageView) view.findViewById(R.id.sofs11);
 
-        SOET1.setOnClickListener(this);
-        SOEVT2.setOnClickListener(this);
-        SOF3.setOnClickListener(this);
-        SOAS4.setOnClickListener(this);
-        SOMC5.setOnClickListener(this);
-        SOP6.setOnClickListener(this);
-        MSSSOA7.setOnClickListener(this);
-        SOABE8.setOnClickListener(this);
-        SOM9.setOnClickListener(this);
-        SOPAHS10.setOnClickListener(this);
-        SOF3.setOnClickListener(this);
+        soet1.setOnClickListener(this);
+        sovet2.setOnClickListener(this);
+        sof3.setOnClickListener(this);
+        soas4.setOnClickListener(this);
+        somc5.setOnClickListener(this);
+        sop6.setOnClickListener(this);
+        msssoa7.setOnClickListener(this);
+        soabe8.setOnClickListener(this);
+        som9.setOnClickListener(this);
+        sopahs10.setOnClickListener(this);
+        sof3.setOnClickListener(this);
         
         return view;
     }
